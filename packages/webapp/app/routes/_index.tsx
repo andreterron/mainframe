184a539803fbc10116d6d533f3c16999562f3095
Andre Terron <git@terron.me>
2023-06-19 14:12:51 -0700

    Create dashboard

--- packages/webapp/app/routes/_index.tsx
+++ packages/webapp/app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { useState } from "react";
+import clsx from "clsx";
 
 export const meta: V2_MetaFunction = () => {
     return [
@@ -8,38 +10,109 @@ export const meta: V2_MetaFunction = () => {
 };
 
 export default function Index() {
+    const [tables, setTables] = useState<{ id: string; name: string }[]>([]);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
     return (
-        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-            <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
-            <ul>
-                <li>
-                    <a
-                        target="_blank"
-                        href="https://remix.run/tutorials/blog"
-                        rel="noreferrer"
-                    >
-                        15m Quickstart Blog Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a
-                        target="_blank"
-                        href="https://remix.run/tutorials/jokes"
-                        rel="noreferrer"
-                    >
-                        Deep Dive Jokes App Tutorial
-                    </a>
-                </li>
-                <li>
-                    <a
-                        target="_blank"
-                        href="https://remix.run/docs"
-                        rel="noreferrer"
-                    >
-                        Remix Docs
-                    </a>
-                </li>
-            </ul>
+        <div>
+            <button
+                data-drawer-target="default-sidebar"
+                data-drawer-toggle="default-sidebar"
+                aria-controls="default-sidebar"
+                type="button"
+                onClick={() => setSidebarOpen(!sidebarOpen)}
+                className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                    className="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                    ></path>
+                </svg>
+            </button>
+
+            <aside
+                id="default-sidebar"
+                className={clsx([
+                    "fixed top-0 left-0 z-40 w-64 h-screen transition-transform sm:translate-x-0 border-r-2 border-gray-400",
+                    sidebarOpen ? "" : "-translate-x-full",
+                ])}
+                aria-label="Sidebar"
+            >
+                <div className="h-full flex flex-col justify-between px-3 py-4 overflow-y-auto bg-white sm:bg-gray-100 dark:bg-gray-800">
+                    <ul className="w-full font-medium flex-shrink">
+                        {tables.map((table) => {
+                            return (
+                                <li key={table.id}>
+                                    <span className="block group py-1 cursor-pointer">
+                                        <span
+                                            className={clsx([
+                                                "flex items-center p-2 rounded-lg",
+                                                "text-gray-900 dark:text-white",
+                                                "group-hover:bg-gray-200 dark:group-hover:bg-gray-700",
+                                            ])}
+                                        >
+                                            <span className="relative">
+                                                {table.name ? (
+                                                    table.name
+                                                ) : (
+                                                    <span className="text-gray-400">
+                                                        Untitled
+                                                    </span>
+                                                )}
+                                            </span>
+                                        </span>
+                                    </span>
+                                </li>
+                            );
+                        })}
+                        <li>
+                            <button
+                                onClick={() => {
+                                    const id = Math.random()
+                                        .toString(36)
+                                        .substring(2, 8);
+                                    setTables((prev) => [
+                                        ...prev,
+                                        { id, name: "" },
+                                    ]);
+                                }}
+                                className="block w-full group py-1 cursor-pointer"
+                            >
+                                <span
+                                    className={clsx([
+                                        "flex w-full items-center p-2 rounded-lg",
+                                        "text-gray-400 dark:text-gray-200",
+                                        "group-hover:bg-gray-200 dark:group-hover:bg-gray-700",
+                                    ])}
+                                >
+                                    <span className="">New Table</span>
+                                </span>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </aside>
+
+            <div
+                onClick={() => setSidebarOpen(false)}
+                className={clsx([
+                    "fixed top-0 left-0 bottom-0 right-0 sm:hidden bg-black transition-opacity z-30",
+                    sidebarOpen
+                        ? "opacity-25"
+                        : "opacity-0 pointer-events-none",
+                ])}
+            />
+
+            <div className="p-4 sm:ml-64">Content</div>
         </div>
     );
 }
--- packages/webapp/tailwind.config.ts
+++ packages/webapp/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
     content: ["./app/**/*.{js,jsx,ts,tsx}"],
+    darkMode: "class",
     theme: {
         extend: {},
     },
