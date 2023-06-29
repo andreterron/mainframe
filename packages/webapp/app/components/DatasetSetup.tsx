9ea9f840bfe3add0fde7c37a38ae72c404ad11a9
Andre Terron <git@terron.me>
2023-06-29 08:36:26 -0700

    Update look of the integration button

--- packages/webapp/app/components/DatasetSetup.tsx
+++ packages/webapp/app/components/DatasetSetup.tsx
@@ -6,9 +6,9 @@ export default function DatasetSetup({
     return (
         <div className="flex flex-col items-start gap-4">
             <h2 className="text-xl">Import</h2>
-            <div>
+            <div className="w-full max-w-3xl grid md:grid-cols-2 lg:grid-cols-3 gap-4">
                 <button
-                    className="border shadow rounded-lg py-1 px-4"
+                    className="block border shadow rounded-lg py-2 px-4"
                     onClick={() => onIntegrationSelected("toggl")}
                 >
                     Toggl
