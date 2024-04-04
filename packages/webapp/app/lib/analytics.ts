9dc27bd6820c434f72812035b4b6f1dd6fa9a950
Andre Terron <git@terron.me>
2024-04-03 21:16:02 -0700

    Fix root awaits

--- packages/webapp/app/lib/analytics.ts
+++ packages/webapp/app/lib/analytics.ts
@@ -1,15 +1,15 @@
 import { env } from "./env_client";
+import posthogJS from "posthog-js";
+import * as Sentry from "@sentry/react";
 
 export const posthog = env.VITE_POSTHOG_KEY
-  ? (await import("posthog-js")).default.init(env.VITE_POSTHOG_KEY, {
+  ? posthogJS.init(env.VITE_POSTHOG_KEY, {
       api_host: "https://app.posthog.com",
     }) ?? undefined
   : undefined;
 
 async function initSentry() {
   if (!env.VITE_SENTRY_DNS) return undefined;
-
-  const Sentry = await import("@sentry/react");
 
   Sentry.init({
     dsn: env.VITE_SENTRY_DNS,
@@ -28,8 +28,8 @@ async function initSentry() {
     replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
-
-  return Sentry;
 }
 
-export const Sentry = await initSentry();
+initSentry();
+
+export { Sentry };
