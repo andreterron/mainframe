8e118526a538acd9f6e89b4cdaa1e0bb312b076a
Andre Terron <git@terron.me>
2024-05-22 14:45:32 -0700

    Update Sentry session replay sample rate

--- packages/webapp/app/lib/analytics.ts
+++ packages/webapp/app/lib/analytics.ts
@@ -25,7 +25,7 @@ async function initSentry() {
     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
     tracePropagationTargets: ["localhost", /^https:\/\/app\.mainframe\.so/],
     // Session Replay
-    replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
 }
