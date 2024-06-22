3295c2d40293f5d6911fdb150f917e457e283d83
Andre Terron <git@terron.me>
2024-06-22 00:08:04 -0700

    Use tsup.config.ts files

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,46 +1,24 @@
-import type { Options } from "tsup";
+import { defineConfig } from "tsup";
 
-export const bannerCJS: Options["banner"] = {};
-
-export const bannerESM: Options["banner"] = {
-  /**
-   * @link https://stackoverflow.com/questions/31931614/require-is-not-defined-node-js
-   */
-  js: `
-   import { createRequire } from 'module';
-   const require = createRequire(import.meta.url);
-  `,
-};
-
-export const options: Options = {
+export default defineConfig({
   entry: ["src/index.ts"],
-  treeshake: true,
-  sourcemap: true,
-  minify: false,
+  format: ["esm", "cjs"],
+  dts: true,
   clean: true,
-  platform: "node",
-  shims: true,
-  ignoreWatch: [
-    "**/.turbo",
-    "**/dist",
-    "**/node_modules",
-    "**/.DS_STORE",
-    "**/.git",
-  ],
-};
 
-export const optionsESM: Options = {
-  ...options,
-  format: "esm",
-  dts: true,
-  splitting: false,
-  banner: bannerESM,
-};
+  // sourcemap: true,
+  // bundle: false,
 
-export const optionsCJS: Options = {
-  ...options,
-  format: "cjs",
-  dts: true,
-  banner: bannerCJS,
-  splitting: false,
-};
+  // splitting: false,
+  // treeshake: true,
+  // minify: false,
+  // platform: "node",
+  // shims: true,
+  // ignoreWatch: [
+  //   "**/.turbo",
+  //   "**/dist",
+  //   "**/node_modules",
+  //   "**/.DS_STORE",
+  //   "**/.git",
+  // ],
+});
