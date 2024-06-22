7a5f2ebb394077c5d54db1a80ba78061cdfafbb6
Andre Terron <git@terron.me>
2024-06-22 01:10:55 -0700

    Add typed-emitter back

--- packages/server/src/lib/operations.ts
+++ packages/server/src/lib/operations.ts
@@ -1,7 +1,10 @@
 import { Operation } from "@mainframe-so/shared";
 import { EventEmitter } from "node:events";
+import type TypedEmitter from "typed-emitter";
 
-export const operations = new EventEmitter();
+export const operations = new EventEmitter() as TypedEmitter<{
+  operation: (operation: Operation) => void;
+}>;
 
 export async function writeOperation(op: Operation) {
   operations.emit("operation", op);
