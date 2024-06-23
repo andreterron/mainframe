032745b8795926e4ceac35f8462b557af668f463
pomdtr <achille.lacoin@gmail.com>
2024-06-23 14:16:23 +0200

    found a solution that works both on deno and nodejs

--- packages/server/src/lib/operations.ts
+++ packages/server/src/lib/operations.ts
@@ -1,9 +1,11 @@
 import { Operation } from "@mainframe-so/shared";
 import { EventEmitter } from "node:events";
-import TypedEventEmitter, { EventMap } from "typed-emitter";
+import { type EventMap } from "typed-emitter";
 
-// workaround from https://github.com/andywer/typed-emitter/issues/39#issuecomment-1607364367
-type TypedEmitter<T extends EventMap> = TypedEventEmitter.default<T>;
+// workaround from https://github.com/andywer/typed-emitter/issues/39#issuecomment-1444130897
+type TypedEmitter<Events extends EventMap> = import("typed-emitter").default<
+  Events
+>;
 
 export const operations = new EventEmitter() as TypedEmitter<{
   operation: (operation: Operation) => void;
