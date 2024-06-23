1fc920c91f463ff07a1af4fae418ec9a5d74d8e0
Andre Terron <git@terron.me>
2024-06-22 22:33:17 -0700

    Update server package entrypoint

--- packages/node/src/hono.ts
+++ packages/node/src/hono.ts
@@ -2,7 +2,7 @@ import { getRequestListener } from "@hono/node-server";
 import { env } from "./lib/env.server";
 import { Env as ApiEnv } from "@mainframe-so/server";
 import express from "express";
-import { createHono } from "@mainframe-so/server";
+import { createMainframeAPI } from "@mainframe-so/server";
 
 type Env = ApiEnv & {
   // Based on import("@hono/node-server").HttpBindings
@@ -12,13 +12,9 @@ type Env = ApiEnv & {
   };
 };
 
-export const hono = createHono<Env>({
-  initApp(app) {
-    // Set the `db` variable
-    app.use(async (c, next) => {
-      c.set("db", c.env.incoming.db);
-      await next();
-    });
+export const hono = createMainframeAPI<Env>({
+  getRequestDB(c) {
+    return c.env.incoming.db;
   },
 });
 
--- packages/server/src/hono.ts
+++ packages/server/src/hono.ts
@@ -1,14 +1,35 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { Env } from "./types.ts";
 import { apiRouter } from "./routers/api-router.ts";
 import { webhookRouter } from "./routers/webhook-router.ts";
+import { LibSQLDatabase } from "drizzle-orm/libsql/driver";
 
-export function createHono<E extends Env = Env>(init?: {
-  initApp: (app: Hono<E>) => void;
+export function createMainframeAPI<E extends Env = Env>(init: {
+  /**
+   * Callback that can be used to add middleware or extra endpoints
+   * to the API
+   * @param app Hono app
+   */
+  initHonoApp?: (app: Hono<E>) => void;
+
+  /**
+   * Callback that returns the database variable for this request
+   *
+   * @param c Hono context. Please don't call response methods.
+   * @returns LibSQLDatabase. Required for some endpoints
+   */
+  getRequestDB: (
+    c: Context<E>,
+  ) => Promise<LibSQLDatabase | undefined> | LibSQLDatabase | undefined;
 }) {
   const app = new Hono<E>();
 
-  init?.initApp?.(app);
+  app.use(async (c, next) => {
+    c.set("db", await init.getRequestDB(c));
+    await next();
+  });
+
+  init.initHonoApp?.(app);
 
   return app
     .route("/api", apiRouter)
