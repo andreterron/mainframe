f7029393747d3e82318fc1c794c3f4ec826dbeea
Achille Lacoin <achille.lacoin@gmail.com>
2024-06-23 17:20:10 +0200

    fix typed-emitter dependency on deno (#53)
    * fix typed-emitter dependency on deno
    
    * found a solution that works both on deno and nodejs

--- packages/server/src/lib/operations.ts
+++ packages/server/src/lib/operations.ts
@@ -1,6 +1,11 @@
 import { Operation } from "@mainframe-so/shared";
 import { EventEmitter } from "node:events";
-import type TypedEmitter from "typed-emitter";
+import { type EventMap } from "typed-emitter";
+
+// workaround from https://github.com/andywer/typed-emitter/issues/39#issuecomment-1444130897
+type TypedEmitter<Events extends EventMap> = import("typed-emitter").default<
+  Events
+>;
 
 export const operations = new EventEmitter() as TypedEmitter<{
   operation: (operation: Operation) => void;
