8000f8f86eacf5d552d50076a75a5aef3c9b387f
Andre Terron <git@terron.me>
2024-06-23 19:26:24 -0700

    Improve build process resiliency

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/**/*.ts"],
   format: ["esm", "cjs"],
-  clean: true,
   sourcemap: true,
   splitting: false,
   minify: false,
@@ -16,6 +15,10 @@ export default defineConfig({
     "**/.git",
   ],
   onSuccess: "tsc --emitDeclarationOnly --declaration",
+
+  // We don't clean in watch mode, because if the IDE or another tsup process
+  // will fail if the files aren't there.
+  clean: false,
 
   // We set `bundle: true`, but we set every file import as an external import,
   // effectively preventing bundling from happening. We do this to rename .ts
