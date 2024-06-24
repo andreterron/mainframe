158965ff65dd136ee28e3d913958d727680acb42
Andre Terron <git@terron.me>
2024-06-23 18:49:48 -0700

    Update tsup

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,24 +1,42 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/**/*.ts"],
   format: ["esm", "cjs"],
-  dts: true,
   clean: true,
+  sourcemap: true,
+  splitting: false,
+  minify: false,
 
-  // sourcemap: true,
-  // bundle: false,
+  ignoreWatch: [
+    "**/.turbo",
+    "**/dist",
+    "**/node_modules",
+    "**/.DS_STORE",
+    "**/.git",
+  ],
+  onSuccess: "tsc --emitDeclarationOnly --declaration",
 
-  // splitting: false,
-  // treeshake: true,
-  // minify: false,
-  // platform: "node",
-  // shims: true,
-  // ignoreWatch: [
-  //   "**/.turbo",
-  //   "**/dist",
-  //   "**/node_modules",
-  //   "**/.DS_STORE",
-  //   "**/.git",
-  // ],
+  // We set `bundle: true`, but we set every file import as an external import,
+  // effectively preventing bundling from happening. We do this to rename .ts
+  // imports to .js
+  bundle: true,
+  esbuildPlugins: [
+    {
+      name: "replace-ts-with-js",
+      setup(build) {
+        build.onResolve({ filter: /\.[jt]sx?$/ }, (args) => {
+          if (args.importer && args.path.match(/\.tsx?$/)) {
+            return {
+              path: args.path.replace(/\.t(sx?)$/, ".j$1"),
+              external: true,
+            };
+          }
+          if (args.kind !== "entry-point") {
+            return { path: args.path, external: true };
+          }
+        });
+      },
+    },
+  ],
 });
