a541ad0db58e6a71daf35056b3419fc028adf279
Andre Terron <git@terron.me>
2024-07-21 22:33:40 -0700

    Add another setup step

--- packages/webapp/app/components/ProjectSetupInstructions.tsx
+++ packages/webapp/app/components/ProjectSetupInstructions.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from "react";
+import { Label } from "./ui/label";
 
 export function ProjectSetupInstructions({
   appId,
@@ -10,9 +11,21 @@ export function ProjectSetupInstructions({
   return (
     <div className="w-[80ch] space-y-4">
       {/* TODO: Copy button */}
+      <div className="flex gap-2 items-center">
+        <div className="size-8 bg-secondary border rounded-full flex items-center justify-center">
+          1
+        </div>
+        <h2 className="font-bold text-lg">Install Mainframe client</h2>
+      </div>
       <pre className="bg-black font-mono p-4 text-white rounded">
         npm i @mainframe-api/react
       </pre>
+      <div className="flex gap-2 items-center !mt-10">
+        <div className="size-8 bg-secondary border rounded-full flex items-center justify-center">
+          2
+        </div>
+        <h2 className="font-bold text-lg">Add MainframeProvider</h2>
+      </div>
       {/* TODO: Copy button */}
       {/* TODO: Syntax highlight */}
       <pre className="bg-black font-mono p-4 text-white rounded">
@@ -33,6 +46,33 @@ function App() {
 }
 
 export default App;`}
+      </pre>
+      <div className="flex gap-2 items-center !mt-10">
+        <div className="size-8 bg-secondary border rounded-full flex items-center justify-center">
+          3
+        </div>
+        <h2 className="font-bold text-lg">Initiate authentication</h2>
+      </div>
+      <pre className="bg-black font-mono p-4 text-white rounded">
+        {`// Home.tsx
+import { useMainframeClient } from "@mainframe-api/react";
+
+export function HomePage() {
+  const mainframe = useMainframeClient();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          mainframe.initiateAuth("github");
+        }}
+      >
+        GitHub
+      </button>
+    </div>
+  );
+}
+`}
       </pre>
       {children}
     </div>
