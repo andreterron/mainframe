a28e8ff3d2de249c1ae84fd6bf1ca83bb04acf56
Michel DeBauge <mdebau@gmail.com>
2024-07-26 15:59:08 -0700

    Update projects index ui

--- packages/webapp/app/webroutes/_app.projects._index.tsx
+++ packages/webapp/app/webroutes/_app.projects._index.tsx
@@ -6,14 +6,53 @@ import { PageBreadcrumb } from "../components/PageBreadcrumb";
 import { BreadcrumbItem, BreadcrumbPage } from "../components/ui/breadcrumb";
 import { PreviewLabel } from "../components/PreviewLabel";
 import { apiClient } from "../lib/api_client";
+import { AppWindow, ChevronRight, Plus } from "lucide-react";
+import { cn } from "~/lib/utils";
 
-function ProjectItem({ children, to }: PropsWithChildren<{ to: To }>) {
+function ProjectItem({
+  className,
+  children,
+  to,
+  appIdTruncatedFirst,
+  appIdTruncatedLast,
+}: PropsWithChildren<{
+  className?: string;
+  to: To;
+  appIdTruncatedFirst?: string;
+  appIdTruncatedLast?: string;
+}>) {
   return (
     <Link
-      className="border flex items-center h-12 w-80 rounded shadow px-3"
+      className={cn(
+        "border flex items-center w-80 rounded-lg shadow px-3 text-sm font-semibold py-3 hover:bg-accent transition",
+        className,
+      )}
       to={to}
     >
-      {children}
+      <div
+        className={`flex items-center justify-center p-2 rounded-full mr-2 ${
+          to === "/projects/new"
+            ? "bg-secondary"
+            : "bg-gradient-to-r from-emerald-200 to-sky-200"
+        }`}
+      >
+        {to === "/projects/new" ? (
+          <Plus className="w-4 h-4" />
+        ) : (
+          <AppWindow className="w-4 h-4" />
+        )}
+      </div>
+      <div className="flex flex-col">
+        {children}
+        {appIdTruncatedFirst && appIdTruncatedLast && (
+          <div className="text-muted-foreground text-xs font-normal">
+            {appIdTruncatedFirst}...{appIdTruncatedLast}
+          </div>
+        )}
+      </div>
+      {to !== "/projects/new" && (
+        <ChevronRight className="w-4 h-4 text-muted-foreground/40 ml-auto" />
+      )}
     </Link>
   );
 }
@@ -49,12 +88,22 @@ export default function ProjectsPage() {
       <div className="p-4 space-y-4">
         {apps?.map((app) => {
           return (
-            <ProjectItem key={app.id} to={`/projects/${app.id}`}>
+            <ProjectItem
+              key={app.id}
+              to={`/projects/${app.id}`}
+              appIdTruncatedFirst={app.id.slice(0, 4)}
+              appIdTruncatedLast={app.id.slice(-4)}
+            >
               {app.name || app.id}
             </ProjectItem>
           );
         })}
-        <ProjectItem to="/projects/new">+</ProjectItem>
+        <ProjectItem
+          className="text-muted-foreground font-normal"
+          to="/projects/new"
+        >
+          Create a new project
+        </ProjectItem>
       </div>
     </div>
   );
