a62fbb01917e5115f2596bd655fd531b28311087
Andre Terron <git@terron.me>
2024-08-07 08:21:21 -0700

    Update auth buttons

--- packages/webapp/app/webroutes/_auth.login.tsx
+++ packages/webapp/app/webroutes/_auth.login.tsx
@@ -101,9 +101,7 @@ export default function AuthLogin() {
           ) : null}
           {authEnabled?.link.enabled ? (
             <Button asChild>
-              <a href={authEnabled.link.loginUrl}>
-                ō&nbsp;&nbsp;&nbsp;Continue with Hellō
-              </a>
+              <a href={authEnabled.link.loginUrl}>Continue to Login</a>
             </Button>
           ) : null}
           <div className="mt-2 text-sm text-rose-700">
