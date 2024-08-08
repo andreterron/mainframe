ca6965ba5a3b076620542a37d89a6f974c90bb07
Andre Terron <git@terron.me>
2024-08-08 10:59:04 -0700

    Undo reverted changes

--- packages/webapp/app/webroutes/_app.projects._index.tsx
+++ packages/webapp/app/webroutes/_app.projects._index.tsx
@@ -6,11 +6,20 @@ import { PageBreadcrumb } from "../components/PageBreadcrumb";
 import { BreadcrumbItem, BreadcrumbPage } from "../components/ui/breadcrumb";
 import { PreviewLabel } from "../components/PreviewLabel";
 import { apiClient } from "../lib/api_client";
+import { cn } from "../lib/utils";
+import { AppWindowIcon, PlusSquareIcon } from "lucide-react";
 
-function ProjectItem({ children, to }: PropsWithChildren<{ to: To }>) {
+function ProjectItem({
+  className,
+  children,
+  to,
+}: PropsWithChildren<{ className?: string; to: To }>) {
   return (
     <Link
-      className="border flex items-center h-12 w-80 rounded shadow px-3"
+      className={cn(
+        "border flex items-center h-12 w-80 rounded shadow px-3",
+        className,
+      )}
       to={to}
     >
       {children}
@@ -50,11 +59,15 @@ export default function ProjectsPage() {
         {apps?.map((app) => {
           return (
             <ProjectItem key={app.id} to={`/projects/${app.id}`}>
+              <AppWindowIcon className="size-4 mr-2" />
               {app.name || app.id}
             </ProjectItem>
           );
         })}
-        <ProjectItem to="/projects/new">+</ProjectItem>
+        <ProjectItem className="text-muted-foreground" to="/projects/new">
+          <PlusSquareIcon className="size-4 mr-2" />
+          New Project
+        </ProjectItem>
       </div>
     </div>
   );
