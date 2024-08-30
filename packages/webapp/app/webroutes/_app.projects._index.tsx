a16dde5d8d41fbd17139160cf6d6bcac4b64b207
Michel DeBauge <mdebau@gmail.com>
2024-08-30 12:26:25 -0700

    Update projects index ui (#61)
    * Update projects index ui
    
    * Make some changes to the ProjectItem component
    
    * Update new project icon
    
    * Update height and icon size
    
    ---------
    
    Co-authored-by: Andre Terron <git@terron.me>

--- packages/webapp/app/webroutes/_app.projects._index.tsx
+++ packages/webapp/app/webroutes/_app.projects._index.tsx
@@ -1,28 +1,54 @@
 import { PageHeader } from "../components/PageHeader";
 import { Link, To } from "react-router-dom";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, ReactNode } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { PageBreadcrumb } from "../components/PageBreadcrumb";
 import { BreadcrumbItem, BreadcrumbPage } from "../components/ui/breadcrumb";
 import { PreviewLabel } from "../components/PreviewLabel";
 import { apiClient } from "../lib/api_client";
 import { cn } from "../lib/utils";
-import { AppWindowIcon, PlusSquareIcon } from "lucide-react";
+import { ChevronRightIcon, AppWindowIcon, PlusIcon } from "lucide-react";
 
 function ProjectItem({
   className,
   children,
   to,
-}: PropsWithChildren<{ className?: string; to: To }>) {
+  icon,
+  appId,
+}: PropsWithChildren<{
+  className?: string;
+  to: To;
+  icon: ReactNode;
+  appId?: string;
+}>) {
   return (
     <Link
       className={cn(
-        "border flex items-center h-12 w-80 rounded shadow px-3",
+        "border flex items-center w-80 h-16 rounded-lg shadow px-3 text-sm font-semibold py-3 hover:bg-accent transition",
         className,
       )}
       to={to}
     >
-      {children}
+      <div
+        className={`flex items-center justify-center p-2 rounded-full mr-2 ${
+          appId
+            ? "bg-gradient-to-r from-emerald-200 to-sky-200"
+            : "bg-secondary"
+        }`}
+      >
+        {icon}
+      </div>
+      <div className="flex flex-col">
+        {children}
+        {appId && (
+          <div className="text-muted-foreground text-xs font-normal">
+            {appId.slice(0, 4)}...{appId.slice(-4)}
+          </div>
+        )}
+      </div>
+      {to !== "/projects/new" && (
+        <ChevronRightIcon className="w-4 h-4 text-muted-foreground/40 ml-auto" />
+      )}
     </Link>
   );
 }
@@ -58,15 +84,22 @@ export default function ProjectsPage() {
       <div className="p-4 space-y-4">
         {apps?.map((app) => {
           return (
-            <ProjectItem key={app.id} to={`/projects/${app.id}`}>
-              <AppWindowIcon className="size-4 mr-2" />
+            <ProjectItem
+              key={app.id}
+              to={`/projects/${app.id}`}
+              appId={app.id}
+              icon={<AppWindowIcon className="size-4" />}
+            >
               {app.name || app.id}
             </ProjectItem>
           );
         })}
-        <ProjectItem className="text-muted-foreground" to="/projects/new">
-          <PlusSquareIcon className="size-4 mr-2" />
-          New Project
+        <ProjectItem
+          className="text-muted-foreground font-normal"
+          to="/projects/new"
+          icon={<PlusIcon className="size-4" />}
+        >
+          Create a new project
         </ProjectItem>
       </div>
     </div>
